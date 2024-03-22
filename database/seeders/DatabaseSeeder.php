2e9dab7ef8eae224ce018cccdb86bc93ddd314f0
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-03-22 10:22:21 +0630

    Project set Up

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Counter;
+use App\Models\Product;
+use App\Models\Customer;
+use App\Models\Invoice;
+use App\Models\InvoiceItem;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Counter::factory(1)->create();
+        Product::factory(5)->create();
+        Customer::factory(5)->create();
+        Invoice::factory(5)->create();
+        InvoiceItem::factory(5)->create();
+    }
+}
