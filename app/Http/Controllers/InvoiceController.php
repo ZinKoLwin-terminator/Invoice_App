8094fd5798e65bbcd98430a4a595aff9743e2cdd
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-03-23 18:47:32 +0630

    Simple Invoice Application - Display all Invoice

---
+++ app/Http/Controllers/InvoiceController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Invoice;
+
+class InvoiceController extends Controller
+{
+    public function get_all_invoices()
+    {
+        $invoices = Invoice::with('customer')->orderBy('id', 'DESC')->get();
+        return response()->json([
+            'invoices' => $invoices
+        ], 200);
+    }
+}
--- resources/js/components/invoices/index.vue
+++ resources/js/components/invoices/index.vue
@@ -1,11 +1,111 @@
 <template>
-  <h1>Index invoice page component</h1>
+  <div class="container">
+    <div class="invoices">
+
+        <div class="card__header">
+            <div>
+                <h2 class="invoice__title">Invoices</h2>
+            </div>
+            <div>
+                <a class="btn btn-secondary">
+                    New Invoice
+                </a>
+            </div>
+        </div>
+
+        <div class="table card__content">
+            <div class="table--filter">
+                <span class="table--filter--collapseBtn ">
+                    <i class="fas fa-ellipsis-h"></i>
+                </span>
+                <div class="table--filter--listWrapper">
+                    <ul class="table--filter--list">
+                        <li>
+                            <p class="table--filter--link table--filter--link--active">
+                                All
+                            </p>
+                        </li>
+                        <li>
+                            <p class="table--filter--link ">
+                                Paid
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="table--search">
+                <div class="table--search--wrapper">
+                    <select class="table--search--select" name="" id="">
+                        <option value="">Filter</option>
+                    </select>
+                </div>
+                <div class="relative">
+                    <i class="table--search--input--icon fas fa-search "></i>
+                    <input class="table--search--input" type="text" placeholder="Search invoice">
+                </div>
+            </div>
+
+            <div class="table--heading">
+                <p>ID</p>
+                <p>Date</p>
+                <p>Number</p>
+                <p>Customer</p>
+                <p>Due Date</p>
+                <p>Total</p>
+            </div>
+
+            <!-- item 1 -->
+
+    <div v-if="invoices.length > 0">
+      <div v-for="item in invoices" :key="item.id" class="table--items">
+        <a href="#" class="table--items--transactionId">#{{ item.id }}</a>
+        <p>{{ item.date }}</p>
+        <p>#{{ item.number }}</p>
+        <p v-if="item.customer">{{ item.customer.firstname}}</p>
+        <p v-else></p>
+        <p>{{ item.due_date }}</p>
+        <p>${{ item.total }}</p>
+      </div>
+    </div>
+    <div class="table-items" v-else>
+      <p>No invoices available.</p>
+    </div>
+  </div>
+        </div>
+
+    </div>
+
+
 </template>
 
 <script>
-export default {
+import { onMounted, ref } from "vue";
+import axios from "axios"; // Import Axios
 
-}
+export default {
+  setup() {
+    let invoices = ref([]);
+
+    onMounted(async () => {
+       getInvoices();
+    });
+
+    const getInvoices = async () => {
+      try {
+        let response = await axios.get("/api/get_all_invoices");
+        invoices.value = response.data.invoices;
+        console.log('response',response);
+      } catch (error) {
+        console.error('Error fetching invoices:', error);
+      }
+    };
+
+    return {
+      invoices
+    };
+  }
+};
 </script>
 
 <style>
